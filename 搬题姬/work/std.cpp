877aea807f04740ef26f012466346f465660b621
fslong <470657570@qq.com>
2026-04-10 15:26:55 +0800

    更新技能

--- 搬题姬/work/std.cpp
+++ 搬题姬/work/std.cpp
@@ -1,11 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+const int N = 100005;
+int n, a[N];
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     
-    // 解题代码
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    
+    sort(a, a + n);
+    int median = a[n / 2];
+    
+    long long moves = 0;
+    for (int i = 0; i < n; i++) {
+        moves += abs(a[i] - median);
+    }
+    
+    cout << moves << '\n';
     
     return 0;
 }
